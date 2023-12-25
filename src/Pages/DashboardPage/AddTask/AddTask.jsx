cc16149cb8130fc1837e04a89a0fcbeb93cae0e9
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-12-25 12:29:28 +0600

    set dashboard layout

--- src/Pages/DashboardPage/AddTask/AddTask.jsx
+++ src/Pages/DashboardPage/AddTask/AddTask.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddTask = () => {
     return (
         <div className='px-4 mt-4'>
-            <h1 className='text-4xl'>Create New Task</h1>
+            <h1 className='text-4xl text-[#0085ad]'>Create New Task</h1>
         </div>
     );
 };
